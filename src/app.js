14de247801de3e86ae81b38822724e3e2c28088c
Daniel Stutzman <dtstutz@gmail.com>
2017-08-11 09:53:49 -0600

    Save results of /api/sync to localStorage

--- src/app.js
+++ src/app.js
@@ -1,13 +1,5 @@
 // @flow
 
-const Ajax = require('./Ajax.js')
-new Ajax().post('http://localhost:3000/api/sync', {})
-  .then((data: any) => {
-    console.log('Successfully did POST', data)
-  }).catch(err => {
-    console.error(`Error from POST: ${err}`)
-  })
-
 let localStorage
 if (process.browser) {
   localStorage = window.localStorage
@@ -15,6 +7,14 @@ if (process.browser) {
   const LocalStorage = require('node-localstorage').LocalStorage
   localStorage = new LocalStorage('./localStorage')
 }
+const Ajax = require('./Ajax.js')
 
-localStorage.setItem('myFirstKey', 'myFirstValue')
-console.log(localStorage.getItem('myFirstKey'))
+const url = 'http://localhost:3000/api/sync'
+console.log(`POST to ${url}...`)
+new Ajax().post(url, {})
+  .then((data: any) => {
+    console.log('POST succeeded')
+    localStorage.setItem('bank', JSON.stringify(data))
+  }).catch(err => {
+    console.error(`Error from POST ${url}: ${err}`)
+  })

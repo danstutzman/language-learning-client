c8d21b7a57c21301345eb53affdab4d1b41538b3
Daniel Stutzman <dtstutz@gmail.com>
2017-08-11 10:01:42 -0600

    Wrap Bank.nextId

---
+++ src/Bank.js
@@ -0,0 +1,15 @@
+// @flow
+
+class Bank {
+  data: { nextId: Number }
+
+  constructor(data: { nextId: Number }) {
+    this.data = data
+  }
+
+  nextId() {
+    return this.data.nextId
+  }
+}
+
+module.exports = Bank
--- src/app.js
+++ src/app.js
@@ -7,13 +7,16 @@ if (process.browser) {
   const LocalStorage = require('node-localstorage').LocalStorage
   localStorage = new LocalStorage('./localStorage')
 }
-const Ajax = require('./Ajax.js')
+const Ajax = require('./Ajax')
+const Bank = require('./Bank')
 
 const url = 'http://localhost:3000/api/sync'
 console.log(`POST to ${url}...`)
 new Ajax().post(url, {})
   .then((data: any) => {
     console.log('POST succeeded')
+    const bank = new Bank(data)
+    console.log('bank.nextId', bank.nextId())
     localStorage.setItem('bank', JSON.stringify(data))
   }).catch(err => {
     console.error(`Error from POST ${url}: ${err}`)

2e8990deb0a54f3e74344a5801933cfd1b796d79
Daniel Stutzman <dtstutz@gmail.com>
2017-08-11 09:23:00 -0600

    Remove warning about wrong node version, since it doesn't run before the "let" error; add to README instead

--- src/app.js
+++ src/app.js
@@ -1,12 +1,6 @@
 // @flow
 
-var REQUIRED_NODE_VERSION = 'v6.11.1'
-if (!process.browser && process.version !== REQUIRED_NODE_VERSION) {
-  throw new Error(`Currently running ${process.version}.  Please first run: nvm use ${REQUIRED_NODE_VERSION} (may need to rerun npm install)`)
-}
-
 const Ajax = require('./Ajax.js')
-
 new Ajax().post('http://localhost:3000/api/sync', {})
   .then(() => {
     console.log('Successfully did POST')

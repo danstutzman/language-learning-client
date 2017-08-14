99d6a9927decfbc63d2f5fc7219a06520fd5527c
Daniel Stutzman <dtstutz@gmail.com>
2017-08-14 14:34:25 -0600

    Moved Redux store into LocalBank

--- src/bank/LocalBank.js
+++ src/bank/LocalBank.js
@@ -1,28 +1,42 @@
-import type { Action } from '../Action'
-import type { BankApi } from './api/BankApi'
-import type { BankApiRequest } from './api/BankApiRequest'
+import type { Action }          from '../Action'
+import type { BankApi }         from './api/BankApi'
+import type { BankApiRequest }  from './api/BankApiRequest'
 import type { BankApiResponse } from './api/BankApiResponse'
-import type { LocalStorage } from '../LocalStorage'
-import SyncedState from './SyncedState'
-import UnsyncedState from './UnsyncedState'
+import type { LocalStorage }    from '../LocalStorage'
+import type { Store }           from 'redux'
+import SyncedState              from './SyncedState'
+import UnsyncedState            from './UnsyncedState'
+import reducer                  from './reducer'
+import { createStore }          from 'redux'
 
 export default class LocalBank {
-  bankApi: BankApi
-  syncedState: SyncedState
+  bankApi:       BankApi
+  syncedState:   SyncedState
   unsyncedState: UnsyncedState
+  reduxStore:    Store<{[actionId: number]: boolean}, Action>
 
   constructor(bankApi: BankApi, localStorage: LocalStorage) {
-    this.bankApi = bankApi
-    this.syncedState = new SyncedState(localStorage)
+    this.bankApi       = bankApi
+    this.syncedState   = new SyncedState(localStorage)
     this.unsyncedState = new UnsyncedState(localStorage)
   }
 
   initFromLocalStorage() {
     this.syncedState.initFromLocalStorage()
     this.unsyncedState.initFromLocalStorage()
+
+    this.reduxStore = createStore(reducer, {})
+    const actions = this.syncedState.actions.concat(this.unsyncedState.actions)
+    for (const action of actions) {
+      this.reduxStore.dispatch(action)
+    }
   }
 
-  sync(): Promise<Array<Action>> {
+  getReduxStoreState() {
+    return this.reduxStore.getState()
+  }
+
+  sync(): Promise<void> {
     const request: BankApiRequest = {
       clientId:                    this.syncedState.clientId,
       clientIdToMaxSyncedActionId: this.syncedState.clientIdToMaxSyncedActionId,
@@ -34,14 +48,19 @@ export default class LocalBank {
       .then(this._handleSyncResponse.bind(this))
   }
 
-  _handleSyncResponse(response: BankApiResponse): Array<Action> {
+  _handleSyncResponse(response: BankApiResponse) {
     console.log(`Sync response: ${JSON.stringify(response)}`)
+
     const newActions = this.syncedState.handleSyncResponse(response)
+    for (const action of newActions) {
+      this.reduxStore.dispatch(action)
+    }
+
     this.unsyncedState.handleSyncResponse(response, this.syncedState.clientId)
-    return newActions
   }
 
-  addAction(): Action {
-    return this.unsyncedState.addAction()
+  addAction() {
+    const action = this.unsyncedState.addAction()
+    this.reduxStore.dispatch(action)
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-import type { Action } from './Action'
 import Ajax            from './Ajax'
 import AjaxBankApi     from './bank/api/AjaxBankApi'
 import App             from './App'
 import LocalBank       from './bank/LocalBank'
 import React           from 'react'
 import ReactDOM        from 'react-dom'
-import reducer         from './reducer'
-import { createStore } from 'redux'
 import { SYNCED_KEY, UNSYNCED_KEY } from '../src/LocalStorage'
 
 const clientId = 1
@@ -26,30 +23,20 @@ const bank = new LocalBank(
   new AjaxBankApi(new Ajax(), 'http://localhost:3000/api/sync'),
   window.localStorage)
 bank.initFromLocalStorage()
-const store = createStore(reducer, {})
-for (const action of bank.syncedState.actions.concat(
-      bank.unsyncedState.actions)) {
-  store.dispatch(action)
-}
 
 function render() {
   ReactDOM.render(
     React.createElement(App, {
-      cards: store.getState(),
+      cards: bank.getReduxStoreState(),
       addCard: ()=>{
-        const action: Action = bank.addAction()
-        store.dispatch(action)
+        bank.addAction()
         render()
       },
       sync: ()=>{
-        bank.sync()
-          .then(newActions => {
-            for (const action of newActions) {
-              store.dispatch(action)
-            }
-            render()
-            console.log('Done syncing')
-          })
+        bank.sync().then(()=>{
+          render()
+          console.log('Done syncing')
+        })
         render()
       }
     }),

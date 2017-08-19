b03183a1f6148e5cc44186cb2b2dfe583460195b
Daniel Stutzman <dtstutz@gmail.com>
2017-08-19 10:33:24 -0600

    Refactor to simplify reducer's case statement

--- src/bank/reducer.js
+++ src/bank/reducer.js
@@ -2,29 +2,29 @@ import type { Action } from './Action'
 import type { AppState } from '../AppState'
 import { assertAddCardAction, assertUpdateCardAction } from './Action'
 
+function addCard(appState: AppState, action: Action) {
+  const add = assertAddCardAction(action)
+  const card = Object.assign({ cardId: action.actionId }, (add.cardAdd: any))
+  appState.cardByCardId[action.actionId] = card
+  appState.fastCards.update()
+  appState.slowCards.update()
+}
+
+function updateCard(appState: AppState, action: Action) {
+  const update = assertUpdateCardAction(action)
+  const card = appState.cardByCardId[update.cardId]
+  Object.assign({}, card, update.cardUpdate)
+  appState.fastCards.update()
+  appState.slowCards.update()
+}
+
 export default function(appState: AppState, action: Action) {
-  let card
   switch (action.type) {
-    case '@@redux/INIT':
-      break // Ignore
-    case 'NOOP':
-      break // Ignore
-    case 'ADD_CARD':
-      var add = assertAddCardAction(action)
-      appState.cardByCardId[action.actionId] =
-        Object.assign({ cardId: action.actionId }, (add.cardAdd: any))
-      appState.fastCards.update()
-      appState.slowCards.update()
-      break
-    case 'UPDATE_CARD':
-      var update = assertUpdateCardAction(action)
-      card = appState.cardByCardId[update.cardId]
-      Object.assign({}, card, update.cardUpdate)
-      appState.fastCards.update()
-      appState.slowCards.update()
-      break
-    default:
-      throw new Error(`Unknown action.type ${action.type}`)
+    case '@@redux/INIT': break // Ignore
+    case 'NOOP':         break // Ignore
+    case 'ADD_CARD':     addCard(appState, action); break
+    case 'UPDATE_CARD':  updateCard(appState, action); break
+    default: throw new Error(`Unknown action.type ${action.type}`)
   }
   return appState
 }

93108425d3aab6d9a5e1a539cba456f0c7e1345c
Daniel Stutzman <dtstutz@gmail.com>
2017-08-18 20:14:15 -0600

    Add 'show mnemonic' button to Slow Quiz

--- src/SlowQuiz.js
+++ src/SlowQuiz.js
@@ -7,21 +7,44 @@ type Props = {
   addExposure:  (Exposure) => void
 }
 
-export default class FastQuiz extends React.Component<void, Props, void> {
+type State = {
+  showMnemonic: boolean
+}
+
+export default class FastQuiz extends React.Component<void, Props, State> {
+  state: State
+
+  constructor() {
+    super()
+    this.state = { showMnemonic: false }
+  }
+
   _onClickIRemember() {
     this.props.addExposure({
       type:   'SLOW_NOD',
       cardId: this.props.topCard.cardId
     })
+    this.setState({ showMnemonic: false })
   }
 
   render() {
-    const en = this.props.topCard.en
+    const topCard = this.props.topCard
     return <div>
-      <p className='en'>{en}</p>
-      <button onClick={this._onClickIRemember.bind(this)}>
+
+      <p className='en'>{topCard.en}</p>
+
+      <button className='big'
+          onClick={()=>{ this.setState({ showMnemonic: true }) }}>
+        Show Mnemonic
+      </button>
+
+      {this.state.showMnemonic && <div>{topCard.mnemonic}</div>}
+
+      <button className='big'
+          onClick={this._onClickIRemember.bind(this)}>
         I Understand
       </button>
+
     </div>
   }
 }
